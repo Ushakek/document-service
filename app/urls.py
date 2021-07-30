1f93c4a7a51d123e41632292f3cb376405c3e40e
Ушаков Никита Сергеевич <uns@zaovip.ru>
2021-07-30 09:34:06 +0500

    Add url
    - Добавлен юрл для создания html документа

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,24 @@
 from fastapi import APIRouter
-from app.servies import report
+from app.servies import create_pdf_report, create_html_report
 from fastapi.responses import HTMLResponse, FileResponse, Response
 
 router = APIRouter()
 
 
-@router.put('/get_pdf', response_class=FileResponse)
-async def get_some():
-    data = report()
+@router.put('/html_report', response_class=FileResponse)
+async def get_html_report(values: dict):
+    data = create_html_report(values)
+    return data
+
+
+@router.get('/help')
+def help_me():
+    return {'some': {'data': 'in data'}, 'next_some': {'data': 'next: data'}}
+
+
+@router.get('/get_pdf', response_class=FileResponse)
+async def get_pdf_report():
+    data = create_pdf_report()
     return data
 
 
