e4e20098b1d561b0d4a44c5e94439b6cbb49256c
Ушаков Никита Сергеевич <uns@zaovip.ru>
2021-07-30 15:26:21 +0500

    Change urls
    - удалены лишние юрлы
    - исправлены нужные юрлы

--- app/urls.py
+++ app/urls.py
@@ -1,24 +1,21 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, File, UploadFile
 from app.servies import create_pdf_report, create_html_report
-from fastapi.responses import HTMLResponse, FileResponse, Response
+from fastapi.responses import FileResponse
 
 router = APIRouter()
 
 
-@router.put('/html_report', response_class=FileResponse)
-async def get_html_report(values: dict):
-    data = create_html_report(values)
+@router.post('/get_html', response_class=FileResponse)
+async def upload_file(values: UploadFile = File(...), file: UploadFile = File(...)):
+    html_template = await file.read()
+    data = await values.read()
+    report_html = create_html_report(template=html_template, values=data)
+    return report_html
+
+
+@router.post('/get_pdf', response_class=FileResponse)
+async def get_pdf_report(values: UploadFile = File(...), file: UploadFile = File(...)):
+    html_template = await file.read()
+    data = await values.read()
+    data = create_pdf_report(values=data, templates=html_template)
     return data
-
-
-@router.get('/help')
-def help_me():
-    return {'some': {'data': 'in data'}, 'next_some': {'data': 'next: data'}}
-
-
-@router.get('/get_pdf', response_class=FileResponse)
-async def get_pdf_report():
-    data = create_pdf_report()
-    return data
-
-
