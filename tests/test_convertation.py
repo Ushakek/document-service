47049824a1ccac2029c5531d147fd2c464edf640
Ушаков Никита Сергеевич <uns@zaovip.ru>
2021-08-04 12:27:37 +0500

    Add tests
    - добавлены тесты на проверку методов

---
+++ tests/test_convertation.py
@@ -0,0 +1,52 @@
+from app.main import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
+
+
+def test_convert_to_original():
+    with open('_data/_reference/reference_report.html', 'rb') as file:
+        reference = file.read()
+
+    response = client.put('/create_report?convert_to=default',
+                          files=[('values', open('_data/_source/data_to_report.json', 'rb')),
+                                 ('file', open('_data/_source/verification.html', 'rb'))])
+
+    with open('_data/_current/report_html.html', 'wb') as file:
+        file.write(response.content)
+
+    assert response.status_code == 200
+    assert response.content == reference
+    print('\nЕсли тест пройден, файл записан, его можно проверить в браузере для проверки наполнения')
+
+
+def test_convert_to_pdf():
+    response = client.put('/create_report?convert_to=pdf',
+                          files=[('values', open('_data/_source/data_to_report.json', 'rb')),
+                                 ('file', open('_data/_source/verification.html', 'rb'))])
+
+    assert response.status_code == 200
+
+    with open('_data/_current/report_pdf.pdf', 'wb') as file:
+        file.write(response.content)
+
+
+def test_error_converting():
+    response = client.put('/create_report?convert_to=pdf',
+                          files=[('values', open('_data/_source/data_to_report.json', 'rb')),
+                                 ('file', open('_data/_source/test.txt', 'rb'))])
+
+    assert response.status_code == 406
+    message = {'detail': 'Не удалось определить формат шаблона. Шаблон должен быть в формате html для конвертации в pdf'}
+    assert response.json() == message
+
+
+def test_convert_to_any_template():
+    with open('_data/_reference/reference_any.txt', 'rb') as file:
+        reference = file.read()
+    response = client.put('/create_report?convert_to=default',
+                          files=[('values', open('_data/_source/data_to_report.json', 'rb')),
+                                 ('file', open('_data/_source/test.txt', 'rb'))])
+
+    assert response.status_code == 200
+    assert reference == response.content
