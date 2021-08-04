80865da96e03f0cdfc9a4141c440187476e160d1
Ушаков Никита Сергеевич <uns@zaovip.ru>
2021-08-04 16:03:57 +0500

    Update tests
    - В связи с последними изменениями, обновлены тесты
    - Добавлены новые тесты
    - Добавлено описание к тестам

--- tests/test_convertation.py
+++ tests/test_convertation.py
@@ -1,16 +1,23 @@
 from app.main import app
 from fastapi.testclient import TestClient
+import json
 
 client = TestClient(app)
 
 
+# Проверка на конвертацию в оригинальный формат
 def test_convert_to_original():
     with open('_data/_reference/reference_report.html', 'rb') as file:
         reference = file.read()
 
-    response = client.put('/create_report?convert_to=default',
-                          files=[('values', open('_data/_source/data_to_report.json', 'rb')),
-                                 ('file', open('_data/_source/verification.html', 'rb'))])
+    with open('_data/_source/data_to_report.json', 'r') as file:
+        values = file.read()
+
+    params = {'values': values}
+
+    response = client.put(url='/create_report',
+                          files=[('template', open('_data/_source/verification.html', 'rb'))],
+                          params=params)
 
     with open('_data/_current/report_html.html', 'wb') as file:
         file.write(response.content)
@@ -20,10 +27,16 @@ def test_convert_to_original():
     print('\nЕсли тест пройден, файл записан, его можно посмотреть для проверки наполнения')
 
 
+# Проверка на конвертацию в pdf
 def test_convert_to_pdf():
+    with open('_data/_source/data_to_report.json', 'r') as file:
+        values = file.read()
+
+    params = {'convert_to': 'pdf', 'values': values}
+
     response = client.put('/create_report?convert_to=pdf',
-                          files=[('values', open('_data/_source/data_to_report.json', 'rb')),
-                                 ('file', open('_data/_source/verification.html', 'rb'))])
+                          files=[('template', open('_data/_source/verification.html', 'rb'))],
+                          params=params)
 
     assert response.status_code == 200
 
@@ -32,22 +45,34 @@ def test_convert_to_pdf():
     print('\nЕсли тест пройден, файл записан, его можно посмотреть для проверки наполнения')
 
 
+# Проверка на ошибку при конвертации в связи с неверно переданными параметрами
 def test_error_converting():
+    with open('_data/_source/data_to_report.json', 'r') as file:
+        values = file.read()
+
+    params = {'convert_to': 'pdf', 'values': values}
+
     response = client.put('/create_report?convert_to=pdf',
-                          files=[('values', open('_data/_source/data_to_report.json', 'rb')),
-                                 ('file', open('_data/_source/test.txt', 'rb'))])
+                          files=[('template', open('_data/_source/test.txt', 'rb'))],
+                          params=params)
 
     assert response.status_code == 406
     message = {'detail': 'Не удалось определить формат шаблона. Шаблон должен быть в формате html для конвертации в pdf'}
     assert response.json() == message
 
 
+# Проверка возможности конвертации в шаблон НЕ HTML
 def test_convert_to_any_template():
+    with open('_data/_source/data_to_report.json', 'r') as file:
+        values = file.read()
+
+    params = {'values': values}
+
     with open('_data/_reference/reference_any.txt', 'rb') as file:
         reference = file.read()
     response = client.put('/create_report?convert_to=default',
-                          files=[('values', open('_data/_source/data_to_report.json', 'rb')),
-                                 ('file', open('_data/_source/test.txt', 'rb'))])
+                          files=[('template', open('_data/_source/test.txt', 'rb'))],
+                          params=params)
 
     assert response.status_code == 200
     assert reference == response.content
@@ -58,16 +83,63 @@ def test_convert_to_any_template():
     print('\nЕсли тест пройден, файл записан, его можно посмотреть для проверки наполнения')
 
 
-def test_any_format_data():
-    with open('_data/_reference/reference_any_data.txt', 'rb') as file:
-        reference = file.read()
+# Проверка на ошибку при конвертации, если записаны неверные значения
+def test_not_valid_values():
+    values = 'some not valid string'
+    params = {'values': values}
 
-    response = client.put('/create_report?convert_to=default',
-                          files=[('values', open('_data/_source/data_to_report.txt', 'rb')),
-                                 ('file', open('_data/_source/test.txt', 'rb'))])
+    response = client.put('/create_report?convert_to=pdf',
+                          files=[('template', open('_data/_source/test.txt', 'rb'))],
+                          params=params)
+
+    assert response.status_code == 406
+    message = {
+        'detail': 'Не верно передан словарь значений. Пожалуйста, посмотрите пример'
+                  ' и повторите ещё раз.'}
+    assert response.json() == message
+
+
+# Проверка на возможность конвертации после кодировки json
+def test_json_format():
+    dict_values = {
+   "column_values": "<tr><td>1</td><td>12345</td><td>some value</td><td>second some value</td><td>Годен</td><td>-</td></tr><tr><td>2</td><td>123456</td><td>some value</td><td>second some value</td><td>Не годен</td><td>Потому что потому, всё кончается на 'У'!</td></tr>",
+    "model": "Smart_S",
+    "date": "date now",
+    "time": "time now"
+ }
+
+    values = json.dumps(dict_values)
+
+    params = {'values': values}
+
+    response = client.put('/create_report?convert_to=pdf',
+                          files=[('template', open('_data/_source/verification.html', 'rb'))],
+                          params=params)
+
+    with open('_data/_current/report_from_json.pdf', 'wb') as file:
+        file.write(response.content)
 
     assert response.status_code == 200
-    assert reference == response.content
 
-    with open('_data/_current/report_any_data.txt', 'wb') as file:
-        file.write(response.content)
+
+# Проверка на ошибку конвертации при использовании не верного формата передачи в requests
+def test_transit_dict():
+    dict_values = {
+        "column_values": "<tr><td>1</td><td>12345</td><td>some value</td><td>second some value</td><td>Годен</td><td>-</td></tr><tr><td>2</td><td>123456</td><td>some value</td><td>second some value</td><td>Не годен</td><td>Потому что потому, всё кончается на 'У'!</td></tr>",
+        "model": "Smart_S",
+        "date": "date now",
+        "time": "time now"
+    }
+
+    params = {'values': dict_values}
+
+    response = client.put('/create_report?convert_to=pdf',
+                          files=[('template', open('_data/_source/verification.html', 'rb'))],
+                          params=params)
+
+    message = {
+        'detail': 'Не верно передан словарь значений. Пожалуйста, посмотрите пример'
+                  ' и повторите ещё раз.'}
+
+    assert response.status_code == 406
+    assert response.json() == message
