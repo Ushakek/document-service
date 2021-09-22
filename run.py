e6adf817687ac3f30209b886acfaab9565bc465b
Ушаков Никита Сергеевич <ushakek@yandex.ru>
2021-09-22 08:38:55 +0500

    Update run file
    - изменён выходной порт в run.py

--- run.py
+++ run.py
@@ -1,12 +1,9 @@
-import os
 import uvicorn
 
 
 def run():
-    reload = os.environ.get('EQUIPMENT_SERVICE_RELOAD') is None
-    host = '0.0.0.0' if not reload else '127.0.0.1'
 
-    uvicorn.run('app.main:app', host=host, port=12345, reload=reload)
+    uvicorn.run('app.main:app', host='0.0.0.0', port=5051, reload=True)
 
 
 if __name__ == '__main__':
